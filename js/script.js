56ec6dc8a71f82697bb96f9828b4bf04a69b5d60
Davide Cesare <davidereaper@gmail.com>
2020-08-26 14:07:02 +0200

    FizzBuzz completato

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var numero = [];
+
+// 1. CICLO "FOR"
+
+// for (var i = 0; i <= 100; i++) {
+//   if (i == 0) {
+//     numero.push("0");
+//   } else if (i % 5 == 0 && i % 3 == 0) {
+//     numero.push("FIZZBUZZ");
+//   } else if (i % 5 == 0) {
+//     numero.push("BUZZ");
+//   } else if (i % 3 == 0) {
+//     numero.push("FIZZ");
+//   } else {
+//     numero.push(i);
+//   }
+//   document.getElementById("lista").innerHTML += "<li>" + numero[i] + "</li>";
+// }
+
+
+// 2. CICLO "WHILE"
+
+// var i = 0;
+// while (i <= 100) {
+//   if (i == 0) {
+//     numero.push("0");
+//   } else if (i % 5 == 0 && i % 3 == 0) {
+//     numero.push("FIZZBUZZ");
+//   } else if (i % 5 == 0) {
+//     numero.push("BUZZ");
+//   } else if (i % 3 == 0) {
+//     numero.push("FIZZ");
+//   } else {
+//     numero.push(i);
+//   }
+//   document.getElementById("lista").innerHTML += "<li>" + numero[i] + "</li>";
+//   i++;
+// }
